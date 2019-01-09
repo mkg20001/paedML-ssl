762f9c4af557bcc00d00fa841b23ea82a43bc027
Maciej Krüger <mkg20001@gmail.com>
2019-01-09 18:58:01 +0100

    fix: username

--- install.sh
+++ install.sh
@@ -31,17 +31,17 @@ npm i
 ./node_modules/.bin/dpl-tool deploy.yaml | bash -
 
 # yes
-PW=$(curl -s 'https://xkpasswd.net/s/index.cgi' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' --data 'a=genpw&n=1&c=%7B%22num_words%22%3A4%2C%22word_length_min%22%3A4%2C%22word_length_max%22%3A8%2C%22case_transform%22%3A%22LOWER%22%2C%22separator_character%22%3A%22-%22%2C%22padding_digits_before%22%3A0%2C%22padding_digits_after%22%3A0%2C%22padding_type%22%3A%22NONE%22%2C%22random_increment%22%3A%22AUTO%22%7D' | jq -r .passwords[0])
+PW=$(curl -s 'https://xkpasswd.net/s/index.cgi' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' --data 'a=genpw&n=1&c=%7B%22num_words%22%3A2%2C%22word_length_min%22%3A4%2C%22word_length_max%22%3A8%2C%22case_transform%22%3A%22LOWER%22%2C%22separator_character%22%3A%22-%22%2C%22padding_digits_before%22%3A0%2C%22padding_digits_after%22%3A0%2C%22padding_type%22%3A%22NONE%22%2C%22random_increment%22%3A%22AUTO%22%7D' | jq -r .passwords[0])
 
-echo "ubuntu:$PW" | chpasswd
+echo "vagrant:$PW" | chpasswd
 
-echo "$PW" > /home/ubuntu/.pw
+echo "$PW" > /home/vagrant/.pw
 
-echo -e "Credentials:\n\tUser:\n\t\tubuntu\n\tPassword:\n\t\t$PW" > /vagrant/credentials
+echo -e "Credentials:\n\tUser:\n\t\tvagrant\n\tPassword:\n\t\t$PW" > /vagrant/credentials
 
 echo "====[ SETUP COMPLETE ]===="
 echo "  User:"
-echo "    ubuntu"
+echo "    vagrant"
 echo "  Password:"
 echo "    $PW"
 echo "=========================="
