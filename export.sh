e611a37a94a205d70eca888fbc8dbd92c1522436
Maciej Krüger <mkg20001@gmail.com>
2019-01-10 09:16:50 +0100

    feat: shorter product desc

--- export.sh
+++ export.sh
@@ -6,6 +6,6 @@ ID=$(cat .vagrant/machines/default/virtualbox/id)
 
 VBoxManage export "$ID" -o dist/paedML-ssl.ovf \
   --vsys 0 \
-  --product "Let's Encrypt Standalone Proxy for paedML" --producturl "https://github.com/mkg20001/paedML-ssl" \
+  --product "Let's Encrypt for paedML" --producturl "https://github.com/mkg20001/paedML-ssl" \
   --vendor "Maciej Krüger (mkg20001)" --vendorurl "https://mkg20001.io" \
   --description "paedML SSL"
