8a23ec04ebd55cdb8c8ff8dd80b26f7753124610
Maciej Krüger <mkg20001@gmail.com>
2019-01-10 10:30:50 +0100

    feat: german keyboard config, vmware fix

--- export.sh
+++ export.sh
@@ -4,6 +4,17 @@ set -e
 
 ID=$(cat .vagrant/machines/default/virtualbox/id)
 
+XML_SOUNDCARD="      <Item>
+        <rasd:AddressOnParent>3</rasd:AddressOnParent>
+        <rasd:AutomaticAllocation>false</rasd:AutomaticAllocation>
+        <rasd:Caption>sound</rasd:Caption>
+        <rasd:Description>Sound Card</rasd:Description>
+        <rasd:ElementName>sound</rasd:ElementName>
+        <rasd:InstanceID>6</rasd:InstanceID>
+        <rasd:ResourceSubType>ensoniq1371</rasd:ResourceSubType>
+        <rasd:ResourceType>35</rasd:ResourceType>
+      </Item>"
+
 VBoxManage export "$ID" -o dist/paedML-ssl.ovf \
   --vsys 0 \
   --product "Let's Encrypt for paedML" --producturl "https://github.com/mkg20001/paedML-ssl" \
@@ -11,4 +22,4 @@ VBoxManage export "$ID" -o dist/paedML-ssl.ovf \
   --description "paedML SSL"
 
 mv dist/paedML-ssl.ovf dist/paedML-ssl-vbox.ovf
-cat dist/paedML-ssl-vbox.ovf | sed "s|<vssd:VirtualSystemType>virtualbox-2.2</vssd:VirtualSystemType>|<vssd:VirtualSystemType>vmx-08</vssd:VirtualSystemType>|g" > dist/paedML-ssl-vmware.ovf
+cat dist/paedML-ssl-vbox.ovf | sed "s|<vssd:VirtualSystemType>virtualbox-2.2</vssd:VirtualSystemType>|<vssd:VirtualSystemType>vmx-08</vssd:VirtualSystemType>|g" | sed "s|$XML_SOUNDCARD||g" > dist/paedML-ssl-vmware.ovf
