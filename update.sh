08f4b2920cad4a43ff1a266aee0ce7f3827009bd
Maciej Krüger <mkg20001@gmail.com>
2019-02-08 15:27:55 +0100

    misc: forgot to commit some stuff

--- update.sh
+++ update.sh
@@ -2,7 +2,20 @@
 
 set -e
 
-rm -rf node_modules
+# because npm
+export SUDO_GID=
+export SUDO_COMMAND=
+export SUDO_USER=
+export SUDO_UID=
+export HOME=/root
+
+# rm node modules and re-install
+rm -rf node_modules package-lock.json
 npm i
+# generate script and deploy
 ./node_modules/.bin/dpl-tool deploy.yaml | bash -
+# clean up node stuff afterwards, only required to compile script
+rm -rf node_modules package-lock.json
+
+# update motd
 echo -e "Let's Encrypt für paedML - Entwickelt von Maciej Krüger\n\nVerwaltung:\n\tsudo proxy-config - Nginx Proxy konfigurieren\n\tsudo proxy-update - Server software aktualisieren\n" > /etc/motd
