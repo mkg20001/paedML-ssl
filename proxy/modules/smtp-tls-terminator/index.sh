2016924268c008d96df8e409305d24c04f47dd1a
Maciej Krüger <mkg20001@gmail.com>
2020-06-04 09:33:59 +0200

    feat: add smtp

---
+++ proxy/modules/smtp-tls-terminator/index.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# @doc:     <plugin-id> <name>
+init_module "smtp"      "SMTP SSL"
+
+smtp_configure() {
+  expose_var SMTP_IP "$(_db smtp_ip)"
+  generate_file "modules/smtp-tls-terminator/template/smtp.conf" "/etc/nginx/stream.d/smtp.conf"
+  
+  ufw allow 993/tcp comment "SMTPS"
+}
+
+smtp_setup() {
+  prompt smtp_ip "SMTPS IP"
+}
+
+smtp_disable() {
+  ufw delete allow 993/tcp
+  rm -f /etc/nginx/stream.d/smtp.conf
+}
