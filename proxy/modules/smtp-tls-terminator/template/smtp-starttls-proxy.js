256e982641494b45a765671ee4da88fd7f88cd23
Maciej Krüger <mkg20001@gmail.com>
2020-06-05 18:43:19 +0200

    feat: work on smtp starttls

--- proxy/modules/smtp-tls-terminator/template/smtp-starttls-proxy.js
+++ proxy/modules/smtp-tls-terminator/template/smtp-starttls-proxy.js
@@ -1,58 +1,100 @@
 'use strict'
 
-const TelCLient = (socket, STARTTLS, onLine, onStarttls) => {
+function splitBuffer (data, thing) {
+  thing = Buffer.from(thing)
+  if (data.indexOf(thing) === -1) return [data]
+  let offset = 0
+  const out = []
+
+  let match
+  while ((match = data.indexOf(thing, offset)) !== -1) {
+    out.push(data.slice(offset, match))
+    offset = match + thing.length
+  }
+
+  if (offset < data.length) {
+    out.push(data.slice(offset))
+  }
+
+  return out
+}
+
+let [target, targetPort, cert, key] = process.argv.slice(2)
+
+targetPort = parseInt(targetPort, 10)
+
+const TelClient = (socket, STARTTLS, onLine, onStarttls) => {
   STARTTLS = STARTTLS.toUpperCase()
-  let starttls = []
   socket.on('data', (data) => {
-    let cmds = data.split('\r\n') // TODO: buffer split
+    console.log(data, String(data))
+    const cmds = splitBuffer(data, '\r\n') // TODO: buffer split
     cmds.forEach((cmd, i) => {
-      if (cmd.toUpperCase().startsWith(STARTTLS)) {
-        let unparsed = cmds.slice(i + 1) // TODO: join \r\n ?
-        socket.pause()
-        onStarttls(socket, unparsed)
+      console.log(i, String(cmd))
+      if (!cmd.length) return
+      if (String(cmd).toUpperCase().startsWith(STARTTLS)) {
+        console.log('START')
         // collect unparsed, concat to buffer
+        let unparsed = cmds.slice(i + 1) // TODO: join \r\n ?
+        if (!unparsed.length) unparsed = Buffer.from('')
+        else if (unparsed.length === 1) unparsed = cmds[0]
+        else unparsed = unparsed.reduce((a, b) => Buffer.concat([a, Buffer.from('\r\n'), b]))
         // pause socket
+        socket.pause()
+        // transmit that line
+        onLine(cmd)
         // call onStarttls
+        onStarttls(socket, unparsed)
       } else {
         onLine(cmd)
       }
     })
   })
+
+  return {
+    writeLine: (b) => socket.write(Buffer.concat([b, Buffer.from("\r\n")]))
+  }
 }
 
 const net = require('net')
 const tls = require('tls')
 const stream = require('stream')
 const sslConfig = require('ssl-config')('modern')
+const fs = require('fs')
+
+const tlsParams = {
+  cert: fs.readFileSync(cert),
+  key: fs.readFileSync(key)
+}
 
 const DefPromise = () => {
   let _resolve
   let _reject
-  new Promise((resolve, reject) => {
+
+  const p = new Promise((resolve, reject) => {
     _resolve = resolve
     _reject = reject
-  }
-  
-  return {
-    resolve: (...a) => _resolve(...a),
-    reject: (...a) => _reject(...a)
-  }
+  })
+
+  p.resolve = (...a) => _resolve(...a)
+  p.reject = (...a) => _reject(...a)
+
+  return p
 }
 
 const TLSMachine = ({ cert, key }) => {
   const server = DefPromise()
   const client = DefPromise()
-  
+
   return {
     setServer: (...a) => server.resolve(a),
     setClient: (...a) => client.resolve(a),
     connect: async () => {
       const [cToM, clientUnparsed] = await client
       const [sToM, serverUnparsed] = await server
-      
+
       const mToS = new stream.Duplex()
       const mToC = new stream.Duplex()
-      
+
       const tlsServer = {
         isServer: true,
         secureContext: tls.createSecureContext({
@@ -65,60 +107,92 @@ const TLSMachine = ({ cert, key }) => {
       }
 
       const tlsClient = {
+        requestCert: true,
         secureContext: tls.createSecureContext({
           ciphers: sslConfig.ciphers,
           honorCipherOrder: true,
           secureOptions: sslConfig.minimumTLSVersion
         })
       }
-      
+
+      console.log("u", clientUnparsed, serverUnparsed)
+
+      mToS.pause()
+      mToC.pause()
+
       const mToSClient = new tls.TLSSocket(mToS, tlsServer)
       const mToCClient = new tls.TLSSocket(mToC, tlsClient)
 
-      mToC.write(clientUnparsed)
+      console.log(mToSClient)
+      console.log(mToSClient.connect)
+
+      mToSClient.connect(sToM)
+      mToCClient.connect(cToM)
+
+      /* mToC.write(clientUnparsed)
       mToS.write(serverUnparsed)
-      
+
+      cToM.pipe(mToC)
+      mToC.pipe(cToM)
+
+      sToM.pipe(mToS)
+      mToS.pipe(sToM) */
+
+
+
       const secToC = DefPromise()
       const secToS = DefPromise()
-      
+
       mToSClient.once('secureConnect', secToS.resolve)
       mToSClient.once('error', secToS.reject)
       mToCClient.once('secureConnect', secToC.resolve)
       mToCClient.once('error', secToC.reject)
-      
-      const client = await secToC
-      const server = await secToS
-      
-      return [client, server]
+
+      const _server = await secToS
+      const _client = await secToC
+
+      console.log("dcon")
+
+      return [_client, _server]
     }
   }
 }
 
-server = net.createServer(async socket => {
-  const tlsm = TLSMachine({})
+const server = net.createServer(async socket => {
+  socket.pause()
+
+  const tlsm = TLSMachine(tlsParams)
   const tsockets = tlsm.connect()
 
-  const client = new Promise((resolve, reject) => {
-    const client = net.connect({ host: target, port: targetPort })
-    client.once('error', reject)
-    client.once('connected', () => {
-      client.fromServer = TelClient(client, "220 2.0.0 Ready to start TLS", async (line) => {
-        fromServer.writeLine(line)
-      }, (socket, unparsed) => {
-        tlsm.setServer(socket, unparsed)
-      })
-      resolve(client)
-    })
-  })
-  
-  const fromClient = TelClient(socket, "STARTTLS", async (line) => {
-    await client
-    client.fromServer.writeLine(line)
+  //TODO: handle end
+
+  const fromClient = TelClient(socket, 'STARTTLS', async (line) => {
+    fromServer = await fromServer
+    fromServer.writeLine(line)
   }, (socket, unparsed) => {
     tlsm.setClient(socket, unparsed)
   })
-  
-  const [client, server] = await tsockets.connect()
+
+  let fromServer = await new Promise((resolve, reject) => {
+    const client = net.connect({ host: target, port: targetPort }, () => {
+      const tClient = TelClient(client, '220 2.0.0 Ready to start TLS', async (line) => {
+        fromClient.writeLine(line)
+      }, (socket, unparsed) => {
+        tlsm.setServer(socket, unparsed)
+      })
+      client.resume()
+      resolve(tClient)
+    })
+    client.once('error', reject)
+  })
+
+  socket.resume()
+
+  console.log('dada')
+
+  const [client, server] = await tsockets
   client.pipe(server)
   server.pipe(client)
 })
+
+server.listen(targetPort + 1000)
